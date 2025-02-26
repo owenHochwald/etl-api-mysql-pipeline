097414a218bcfed577ce4d7ff2363f8f8b730e7e
owenHochwald <owenhochwald@gmail.com>
2025-02-26 15:59:48 -0800

    implement extract.py

--- src/api/fetch_data.py
+++ src/api/fetch_data.py
@@ -8,14 +8,11 @@ load_dotenv()
 
 def fetch_nba_data():
 
-    # url = "https://api-nba-v1.p.rapidapi.com/players/statistics"
     url = "https://api-nba-v1.p.rapidapi.com/players"
     api_key = os.environ['API_KEY']
 
 
-    # querystring = {"game":"8133"}
-    querystring = {"country":"USA", "season" : 2021}
-    # querystring = {"season":2021}
+    querystring = {"country":"USA"}
 
     headers = {
         "x-rapidapi-key": api_key,
---
+++ src/elt/extract.py
@@ -0,0 +1,11 @@
+from ..api.fetch_data import fetch_nba_data
+import requests
+
+def extract_data():
+    """Fetches NBA game data from the API and selects info out."""
+    response = fetch_nba_data()
+    return response.get('response')
+
+
+print(extract_data())
+
