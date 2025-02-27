beda1b4a116ac5788c97ac09409b9d1e716b0f2f
owenHochwald <owenhochwald@gmail.com>
2025-02-26 17:50:31 -0800

    update tests and add absolute pathing

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -5,28 +5,53 @@ from src.etl.transform import transform_data
 def test_extract_data():
     """Test that extract_data fetches NBA game data."""
     data = extract_data()
-    assert isinstance(data, dict)  # Data should be a dictionary
-    assert "response" in data  # The "response" key should exist
-    assert isinstance(data["response"], list)  # Should be a list of players
+    assert isinstance(data, list)  # Data should be a dictionary
+    assert 2 == data[:][0]['id']  # Checking if first player has id element
 
 def test_transform_data():
     """Test that transform_data cleans and processes the data correctly."""
-    sample_data = {
-        "response": [
-            {
-                "id": 1,
-                "date": "2023-02-15",
-                "home_team": {"id": 10, "full_name": "Lakers"},
-                "visitor_team": {"id": 20, "full_name": "Bulls"},
-                "home_team_score": 110,
-                "visitor_team_score": 105
+    sample_data = [{
+        'id': 2,
+        'firstname': 'Quincy',
+        'lastname': 'Acy',
+        'birth': {
+            'date': '1990-10-06',
+            'country': 'USA'
+        },
+        'nba': {
+            'start': 2012,
+            'pro': 6
+        },
+        'height': {
+            'feets': '6',
+            'inches': '7',
+            'meters': '2.01'
+        },
+        'weight': {
+            'pounds': '240',
+            'kilograms': '108.9'
+        },
+        'college': 'Baylor',
+        'affiliation': 'Baylor/USA',
+        'leagues': {
+            'standard': {
+                'jersey': 4,
+                'active': True,
+                'pos': 'F'
             }
-        ]
-    }
+        }
+    }]
 
     transformed = transform_data(sample_data)
     
     assert isinstance(transformed, list)
     assert len(transformed) == 1
-    assert transformed[0]["home_team"] == "Lakers"
-    assert transformed[0]["visitor_team"] == "Bulls"
+    
+    player = transformed[0]
+    assert player["player_id"] == 2
+    assert player["name"] == "Quincy Acy"
+    assert player["nba_start_year"] == 2012
+    assert player["height"] == '2.01'
+    assert player["weight"] == '108.9'
+    assert player["position"] == "F"
+    assert player["jersey_number"] == 4
